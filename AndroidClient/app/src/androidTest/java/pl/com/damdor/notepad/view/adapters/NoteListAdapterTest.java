40a941f2f91bfbd3c6ac814ba8d1c45e1b51c83a
Damian Doroba <damdor.dev@gmail.com>
2019-03-12 18:12:47 +0100

    NoteListAdapterTest

---
+++ AndroidClient/app/src/androidTest/java/pl/com/damdor/notepad/view/adapters/NoteListAdapterTest.java
@@ -0,0 +1,44 @@
+package pl.com.damdor.notepad.view.adapters;
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import pl.com.damdor.notepad.R;
+import pl.com.damdor.notepad.data.Note;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Damian Doroba on 2019-03-12.
+ */
+public class NoteListAdapterTest {
+
+    private static final List<Note> TEST_NOTES = Arrays.asList(
+            Note.create(1, "title1", "content1"),
+            Note.create(2, "title2", "content2"),
+            Note.create(3, "title3", "content3")
+    );
+
+    private NoteListAdapter mAdapter;
+
+    @Before
+    public void setup(){
+        mAdapter = new NoteListAdapter(InstrumentationRegistry.getInstrumentation().getTargetContext(), TEST_NOTES);
+    }
+
+    @Test
+    public void testView(){
+        View view = mAdapter.getView(0, null, null);
+
+        TextView titleText = view.findViewById(R.id.item_note_title);
+        assertEquals(TEST_NOTES.get(0).getTitle(), titleText.getText());
+    }
+
+}
